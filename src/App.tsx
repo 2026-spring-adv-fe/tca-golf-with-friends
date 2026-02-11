0468027056b43cf051d63f4322dee99f3562d3f9
Khaun02 <khaunx02@gmail.com>
2026-02-11 13:29:05 -0600

    Added cupcake theme and dock

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,8 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="p-3">
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1 className="text-3xl underline font-bold">Vite + React</h1>
+        <div className="p-3" data-theme='cupcake'>
+            <h1 className="text-3xl underline font-bold text-center">Golf With Your Friends</h1>
             <div className="card">
                 <button
                     onClick={() => setCount((count) => count + 1)}
@@ -28,13 +16,7 @@ function App() {
                 >
                     count is {count}
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
 
             <div className="card bg-base-100 w-full shadow-lg my-5">
                 <div className="card-body p-2">
@@ -403,6 +385,120 @@ function App() {
                         </table>
                     </div>
                 </div>
+            </div>
+
+            <div className="dock bg-neutral text-neutral-content">
+                <button className="dock-active">
+                    <svg
+                        className="size-[1.2em]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                    >
+                        <g
+                            fill="currentColor"
+                            strokeLinejoin="miter"
+                            strokeLinecap="butt"
+                        >
+                            <polyline
+                                points="1 11 12 2 23 11"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></polyline>
+                            <path
+                                d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="square"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></path>
+                            <line
+                                x1="12"
+                                y1="22"
+                                x2="12"
+                                y2="18"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="square"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></line>
+                        </g>
+                    </svg>
+                    <span className="dock-label">Home</span>
+                </button>
+
+                <button className="">
+                    <svg
+                        className="size-[1.2em]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                    >
+                        <g
+                            fill="currentColor"
+                            strokeLinejoin="miter"
+                            strokeLinecap="butt"
+                        >
+                            <polyline
+                                points="3 14 9 14 9 17 15 17 15 14 21 14"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></polyline>
+                            <rect
+                                x="3"
+                                y="3"
+                                width="18"
+                                height="18"
+                                rx="2"
+                                ry="2"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="square"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></rect>
+                        </g>
+                    </svg>
+                    <span className="dock-label">Setup</span>
+                </button>
+
+                <button>
+                    <svg
+                        className="size-[1.2em]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                    >
+                        <g
+                            fill="currentColor"
+                            strokeLinejoin="miter"
+                            strokeLinecap="butt"
+                        >
+                            <circle
+                                cx="12"
+                                cy="12"
+                                r="3"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="square"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></circle>
+                            <path
+                                d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="square"
+                                stroke-miterlimit="10"
+                                strokeWidth="2"
+                            ></path>
+                        </g>
+                    </svg>
+                    <span className="dock-label">Play</span>
+                </button>
             </div>
         </div>
     );
