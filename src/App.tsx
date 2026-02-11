2962fece206c949bb5e48575c308f39025f84a3d
Khaun02 <khaunx02@gmail.com>
2026-02-11 13:40:40 -0600

    Updated table to remove the table footer

--- src/App.tsx
+++ src/App.tsx
@@ -371,17 +371,6 @@ function App() {
                                     <td>Red</td>
                                 </tr>
                             </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th></th>
-                                    <th>Name</th>
-                                    <th>Job</th>
-                                    <th>company</th>
-                                    <th>location</th>
-                                    <th>Last Login</th>
-                                    <th>Favorite Color</th>
-                                </tr>
-                            </tfoot>
                         </table>
                     </div>
                 </div>
