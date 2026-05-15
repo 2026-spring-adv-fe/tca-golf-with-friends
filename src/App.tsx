498ef91369cfee21ddb9902d4fea59f40372340b
Shaun Khang <khaunx02@gmail.com>
2026-05-15 12:33:09 -0500

    Got routing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { HashRouter, Routes, Route } from "react-router";
+
+import Home from "./Home.tsx";
+import Setup from "./Setup.tsx";
+import Play from "./Play.tsx";
 
 const App = () => {
-  return <div>Cleared everything out</div>;
+  return (
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 };
 
 export default App;
